ab34f059606d93083a6cfe292239306224a9d6a8
Daniela Plascencia <daplascen@gmail.com>
2016-04-23 15:25:33 +0000

    DnPlas: Added content
    Added content to main.py and lcd_display.py
    
    Signed-off-by: Daniela Plascencia <daplascen@gmail.com>

--- InternetOfThings101/main.py
+++ InternetOfThings101/main.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python
 
+import psutil
 import signal
 import sys
 import time
 
+from threading import Thread
+
 def interruptHandler(signal, frame):
-    sys.exit(0)
-
+        sys.exit(0)
+def dataNetwork():
+        netdata = psutil.net_io_counters()
+        return netdata.packets_sent + netdata.packets_recv
+def dataNetworkHandler():
+        idDevice = "IoT101Device"
+        while True:
+            packets = dataNetwork()
+            message = idDevice + " " + str(packets)
+            print "dataNetworkHandler " + message
+            time.sleep(1)
 if __name__ == '__main__':
+        signal.signal(signal.SIGINT, interruptHandler)
+        threadx = Thread(target=dataNetworkHandler)
+        threadx.start()
 
-    signal.signal(signal.SIGINT, interruptHandler)
+        while True:
+            print "Hello Internet of Things 101"
+            time.sleep(5)
 
-    while True:
-        print "Hello Internet of Things 101"
-        time.sleep(5)
-
-# End of File
+#End of file
